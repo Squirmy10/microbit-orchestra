2b4f9ba4316cf69d7004b74a96f05ca123fb9a74
Nic Koenig <richteresanic@gmail.com>
2026-02-23 19:37:43 -0800

    Update pxt.json, test.ts, test.ts

--- test.ts
+++
@@ -1,9 +0,0 @@
-// Sets the tempo to 140 BPM
-Orchestra.setOrchestraTempo(140)
-
-// Wait for the conductor signal '1' to start playing
-Orchestra.onSignalReceived(1, function () {
-    Orchestra.playOrchestraNote(Note.C, NoteDuration.Quarter, Dynamic.MF)
-    Orchestra.playOrchestraNote(Note.E, NoteDuration.Quarter, Dynamic.F)
-    Orchestra.playOrchestraNote(Note.G, NoteDuration.Half, Dynamic.FF)
-})

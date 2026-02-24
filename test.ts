99271fe5c12090a3ebf2d591babf9276b453fda2
Nic Koenig <richteresanic@gmail.com>
2026-02-23 19:34:16 -0800

    Add orchestral tempo setting and note playing logic

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+// Sets the tempo to 140 BPM
+Orchestra.setOrchestraTempo(140)
+
+// Wait for the conductor signal '1' to start playing
+Orchestra.onSignalReceived(1, function () {
+    Orchestra.playOrchestraNote(Note.C, NoteDuration.Quarter, Dynamic.MF)
+    Orchestra.playOrchestraNote(Note.E, NoteDuration.Quarter, Dynamic.F)
+    Orchestra.playOrchestraNote(Note.G, NoteDuration.Half, Dynamic.FF)
+})
